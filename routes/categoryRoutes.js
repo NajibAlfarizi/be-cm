902d3430ccab7f1a9386c30778ad307036b6ea07
Najib Alfarizi <najibalfarizi5@gmail.com>
2025-04-23 14:05:23 +0700

    fix: remove unnecessary authentication middleware from category retrieval routes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -12,10 +12,10 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.get('/', getCategories);
-router.get('/:id', protect, admin, getCategoryById);
+router.get('/:id', getCategoryById);
 router.post('/', protect, admin, createCategory);
 router.put('/:id', protect, admin, updateCategory);
 router.delete('/:id', protect, admin, deleteCategory);
-router.get('/name/:name', protect, admin, getCategoryByName);
+router.get('/name/:name', getCategoryByName);
 
 export default router;
